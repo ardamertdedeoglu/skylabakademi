51cdc9a4402b77b29d333a3754cf4bc97a2ef3da
Arda Mert Dedeoğlu <ardamertsot@hotmail.com>
2024-10-31 16:50:05 +0300

    Add files via upload

---
+++ akademi-main/src/main/java/com/skylab/akademi/entities/Restaurant.java
@@ -0,0 +1,31 @@
+package com.skylab.akademi.entities;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "restaurants")
+public class Restaurant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "restaurant", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<RestaurantTable> tables;
+}
